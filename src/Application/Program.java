29339ec26daf10c24beb369448f59a082f174d6e
Guilherme Serra <guilhermeserra99@gmail.com>
2023-08-03 20:32:28 -0300

    Department entity class

---
+++ src/Application/Program.java
@@ -0,0 +1,12 @@
+package Application;
+
+import model.entites.Departament;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Departament obj = new Departament(1, "Books");
+        System.out.println(obj);
+        
+    }
+}
